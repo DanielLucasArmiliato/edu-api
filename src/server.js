c7e24686e2cd53c8e5b056bc4495fc1889931415
Daniel Lucas <daniarmiliato@gmail.com>
2022-07-21 21:02:10 -0300

    corrige ambiente

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
-const PORT = 4000;
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes')
+const PORT = 4000
 
-const app = express();
+const app = express()
 
-app.use(cors({
-  credentials: true,
-  origin: true
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin: true
+  })
+)
 
 app.options('*', cors())
 
-app.use(cors());
-app.use(express.json());
+app.use(express.json())
 app.use(routes)
-app.listen(process.env.PORT, () => {
-  console.log(`API education is on ${PORT}`);
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`API education is on ${PORT}`)
 })
